b6dfec2f79163aec898fd023dfa123beade41feb
Enrique Flores <ehf.81@hotmail.com>
2021-02-14 22:40:42 -0800

    edit

--- js/ex1.js
+++ js/ex1.js
@@ -6,16 +6,16 @@ function calculator() {
   let num2 = prompt("Select another number")
   let mathop = prompt("Select a math operation: '+', '-', '*', or '/'");
     if (mathop === '+') {
-      console.log(`${num1} + ${num2} =` (num1 + num2));
+      return(`${num1} + ${num2} =` (num1 + num2));
     }
     if (mathop === '-') {
-      console.log(`${num1} - ${num2} =` (num1 - num2));
+      return(`${num1} - ${num2} =` (num1 - num2));
     }
     if (mathop === '*') {
-      console.log(`${num1} * ${num2} =` (num1 * num2));
+      return(`${num1} * ${num2} =` (num1 * num2));
     }
     if (mathop === '/') {
-      console.log(`${num1} / ${num2} =` (num1 / num2));
+      return(`${num1} / ${num2} =` (num1 / num2));
     }
    document.getElementById("example").innerHTML = text;
   }
