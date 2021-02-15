3c92f60b82bd83b396d23844f582b27f25d45cec
Enrique Flores <ehf.81@hotmail.com>
2021-02-14 23:48:05 -0800

    edit

--- js/ex1.js
+++ js/ex1.js
@@ -1,23 +1,27 @@
 /* Calculator */
 
 function calculator() {
-  var text;
+  
   let num1 = Number(prompt("Select a number:"));
   let num2 = Number(prompt("Select another number"));
-  let mathop = prompt("Select a math operation: '+', '-', '*', or '/'");
-    if (mathop === '+') {
-      return(`${num1} + ${num2} =` (num1 + num2));
-    }
-    if (mathop === '-') {
-      return(`${num1} - ${num2} =` (num1 - num2));
-    }
-    if (mathop === '*') {
-      return(`${num1} * ${num2} =` (num1 * num2));
-    }
-    if (mathop === '/') {
-      return(`${num1} / ${num2} =` (num1 / num2));
-    }
-   
-  }
+  let mathop = prompt("Select a math operation: '+' , '-' , '*' , or '/'");
+  switch(mathop) {
+   case "+":
+     Number(prompt(`${num1} + ${num2} = `, (num1+num2)));
+     break;
+   case "-":
+     Number(prompt(`${num1} - ${num2} = `, (num1-num2)));
+     break;
+   case "*":
+     Number(prompt(`${num1} * ${num2} = `, (num1*num2)));
+     break;
+   case "/":
+     Number(prompt(`${num1} / ${num2} = `, (num1/num2)));
+     break;
+   default:
+     text = "I have never heard of that one..";
+   }
+   /*document.getElementById("example").innerHTML = text; */
+ }
  
   console.log(calculator())
