a174c263db383556dcd2bc5e1600fd68f2dc5916
Enrique Flores <ehf.81@hotmail.com>
2021-02-14 22:48:25 -0800

    edit

--- js/ex1.js
+++ js/ex1.js
@@ -2,8 +2,8 @@
 
 function calculator() {
   var text;
-  let num1 = prompt("Select a number:")
-  let num2 = prompt("Select another number")
+  let num1 = prompt("Select a number:");
+  let num2 = prompt("Select another number");
   let mathop = prompt("Select a math operation: '+', '-', '*', or '/'");
     if (mathop === '+') {
       return(`${num1} + ${num2} =` (num1 + num2));
@@ -17,7 +17,7 @@ function calculator() {
     if (mathop === '/') {
       return(`${num1} / ${num2} =` (num1 / num2));
     }
-   document.getElementById("example").innerHTML = text;
+   
   }
  
   console.log(calculator())
