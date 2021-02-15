942a3696d62fe1dce4e1d0d01d7a948e643cf8a2
Enrique Flores <ehf.81@hotmail.com>
2021-02-14 23:53:58 -0800

    edit

--- js/ex1.js
+++ js/ex1.js
@@ -3,8 +3,8 @@
 function calculator() {
   
   let num1 = Number(prompt("Select a number:"));
-  let num2 = Number(prompt("Select another number"));
-  let mathop = prompt("Select a math operation: '+' , '-' , '*' , or '/'");
+  let num2 = Number(prompt("Select another number:"));
+  let mathop = prompt("Select a math operation: ' + ' , ' - ' , ' * ' , or ' / '");
   switch(mathop) {
    case "+":
      Number(prompt(`${num1} + ${num2} = `, (num1+num2)));
