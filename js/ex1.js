a6b78dff8760941d1f044a62c29ed16cc8594283
Enrique Flores <ehf.81@hotmail.com>
2021-02-14 22:54:42 -0800

    num

--- js/ex1.js
+++ js/ex1.js
@@ -2,8 +2,8 @@
 
 function calculator() {
   var text;
-  let num1 = prompt("Select a number:");
-  let num2 = prompt("Select another number");
+  let num1 = Number(prompt("Select a number:"));
+  let num2 = Number(prompt("Select another number"));
   let mathop = prompt("Select a math operation: '+', '-', '*', or '/'");
     if (mathop === '+') {
       return(`${num1} + ${num2} =` (num1 + num2));
