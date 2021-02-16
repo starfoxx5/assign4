e8ffa0314791c113d4ab0600462479870c09c85b
Enrique Flores <ehf.81@hotmail.com>
2021-02-15 16:02:04 -0800

    edit

---
+++ js/ex4.js
@@ -0,0 +1,17 @@
+/* Word List */
+
+const words = [];
+
+let word = "";
+while (word !== "stop") {
+  word = prompt("Enter a word or type 'stop' to quit:");
+  if (word !== "stop") {
+    words.push(word);
+    
+  }
+}
+
+console.log("You have entered the following words:");
+words.forEach(w => {
+  console.log(w);
+});
