94b60e4e64b50d9c9f3b2c2b646404d2fd050061
Enrique Flores <ehf.81@hotmail.com>
2021-02-15 18:36:53 -0800

    edit

--- js/ex6.js
+++ js/ex6.js
@@ -1,22 +1,32 @@
+/* Random Number */
+
+
+let guessCount = 1
 function guessNumber() {
 
-  // generating a random integer from 1 to 10
-  const random = Math.floor(Math.random() * 10) + 1;
+  // generating a random integer from 1 to 100
+  const random = Math.floor(Math.random() * 100) + 1;
 
   // take input from the user
-  let number = parseInt(prompt('Guess a number from 1 to 10: '));
+  let number = parseInt(prompt('Guess a number from 1 to 100: '));
 
   // take the input until the guess is correct
   while(number !== random) {
-      number = parseInt(prompt('Guess a number from 1 to 10: '));
-  }
+      number = parseInt(prompt('Guess a number from 1 to 100: '));
+      guessCount += 1; {
+      if (number < random) {
+        console.log("You're number is too low. Guess again.");}
+      else if (number > random) {
+        console.log("You're number is too high. Guess again.");}
+      }
+  
 
   // check if the guess is correct
   if(number == random) {
-      console.log('You guessed the correct number.');
+      console.log(`Correct! It took you ${guessCount} attempt(s) to guess the correct number`);
   }
 
-}
+}}
 
 // call the function
 guessNumber();
